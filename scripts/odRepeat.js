96b5b9830f13def1d7c59014a42a4a147ee81731
Gabriel Ferreira <Gabrielferrer@outlook.com.br>
2018-01-16 21:10:07 -0200

    Refatoração do odRepeat

--- scripts/odRepeat.js
+++ scripts/odRepeat.js
@@ -52,26 +52,23 @@ function alteraBoleana() {
 }
 
 function _odRepeat() {
-  var odRepeat = document.querySelectorAll('[od-repeat]'); // Pegando todo os atributos od-repeat
-  odRepeat.forEach((item) => { // Pegando cada um dos atributos od-repeat
-    // console.log(item);
-    let listaArray = eval(item.getAttribute('od-repeat')); // Pegando o eval do od-repeat
-    let odRepeatValue = item.querySelectorAll('[od-repeat-value]'); // Pegando todos os od-repeat-value
-    listaArray.forEach((listaItem) => { // Acessando cada item do array
-      let newItem =  item.cloneNode(true); // Clonando
-      // console.log(newItem);
-        newItem.innerHTML = ''; // Limpando novo elemento
-        odRepeatValue.forEach((valueRepeat) => {
-        let newodRepeatValue = valueRepeat.cloneNode(true); // Clonando o elemento para que possamos trabalhar em elementos diferentes
-        let value = listaItem[valueRepeat.getAttribute('od-repeat-value')]; // Salvando o valor dentro da variavel
-        value = value ? value : ''; // Verifica se é undefined
-        newodRepeatValue.innerHTML = value; // Salvando o valor dentro do elemento
-        newItem.appendChild(newodRepeatValue); // Adicionando os elementos dentro do item
+    let odRepeat = document.querySelectorAll('[od-repeat]');
+    odRepeat.forEach((item) => {
+      let itemModel = [];
+      let models = item.querySelectorAll('[od-repeat-value]');
+      models.forEach((model) => {
+        itemModel.push(model.getAttribute('od-repeat-value'));
       });
-      item.parentNode.appendChild(newItem); // Adicionando itens
+      eval(item.getAttribute('od-repeat')).forEach((itemList) => {
+        let newItem = item.cloneNode(true);
+        itemModel.forEach((itemModelList) => {
+          let searchQuery = `[od-repeat-value=${itemModelList}]`;
+          newItem.querySelector(searchQuery).innerHTML = itemList[itemModelList] || '';
+          
+        });
+          item.parentNode.appendChild(newItem);
+      });
     });
-    item.parentNode.removeChild(item); // Tirando o item desnecessario que fica no topo
-  });
   }
 
 
